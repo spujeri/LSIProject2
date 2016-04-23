b955f5fce592a7a73aeb1ea7915918bc99e8a7c6
mihirshah91 <mihir.5191@gmail.com>
2016-04-22 22:33:43 -0400

    test file
    Change-Id: I9b24dc2cb2c347e8815eaabcdc9312c80089621a
    Signed-off-by: mihirshah91 <mihir.5191@gmail.com>

---
+++ LSIMapReduce/src/main/java/com/cornell/cs5300/mapreduce/main/Test.java
@@ -0,0 +1,17 @@
+package com.cornell.cs5300.mapreduce.main;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = 1;
+		double node = (1/685230.0) ;
+		System.out.println("node = " + node);
+		
+		int num1 = (int) Math.ceil(num / 10000.0);
+		System.out.println(num1);
+
+	}
+
+}
