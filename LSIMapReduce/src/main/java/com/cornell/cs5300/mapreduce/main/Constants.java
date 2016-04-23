ec5509a74bdf93db371a062813cb1fa274def3b6
mihirshah91 <mihir.5191@gmail.com>
2016-04-22 22:23:22 -0400

    Change-Id: I48cc9c921d7a63611361da27544b1a653fa15005

--- LSIMapReduce/src/main/java/com/cornell/cs5300/mapreduce/main/Constants.java
+++ LSIMapReduce/src/main/java/com/cornell/cs5300/mapreduce/main/Constants.java
@@ -2,7 +2,9 @@ package com.cornell.cs5300.mapreduce.main;
 
 public abstract class Constants {
 	
-	public static final  String EDGES_PATH = "/Users/Shiva/Documents/Masters/Assignements/LSI/proj2/edgestest.txt";
-	public static final String BLOCK_PATH = "/Users/Shiva/Documents/Masters/Assignements/LSI/proj2/blocks.txt";
+
+	
+	public static final  String EDGES_PATH = "/Users/mihir/Documents/MEng folder new/LSI/Project2/input/edgestest.txt";
+	public static final String BLOCK_PATH = "/Users/mihir/Documents/MEng folder new/LSI/Project2/input/blocks.txt";
 
 }
