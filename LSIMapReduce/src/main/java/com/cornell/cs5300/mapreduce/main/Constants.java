f617e498a6fc082b78c7b17234f822069e246590
mihirshah91 <mihir.5191@gmail.com>
2016-04-23 23:10:36 -0400

    Change-Id: I3568efa92a23d570c3bb65eb0a6ab92709848d46

--- LSIMapReduce/src/main/java/com/cornell/cs5300/mapreduce/main/Constants.java
+++ LSIMapReduce/src/main/java/com/cornell/cs5300/mapreduce/main/Constants.java
@@ -2,15 +2,19 @@ package com.cornell.cs5300.mapreduce.main;
 
 public abstract class Constants {
 	
-	public static final String FILTERED_EDGES = "/Users/Shiva/Documents/Masters/Assignements/LSI/proj2/filteredEdges.txt";
-	public static final String FILTERED_EDGES_DEGREE = "/Users/Shiva/Documents/Masters/Assignements/LSI/proj2/filteredEdgesAdjList.txt";
+	//public static final String FILTERED_EDGES = "/Users/Shiva/Documents/Masters/Assignements/LSI/proj2/filteredEdges.txt";
+	//public static final String FILTERED_EDGES_DEGREE = "/Users/Shiva/Documents/Masters/Assignements/LSI/proj2/filteredEdgesAdjList.txt";
 	public static final double initilaPR = 1/685320.0;
 	
-//	public static final  String EDGES_PATH = "/Users/mihir/Documents/MEng folder new/LSI/Project2/input/edgestest.txt";
-	//public static final String BLOCK_PATH = "/Users/mihir/Documents/MEng folder new/LSI/Project2/input/blocks.txt";
+	public static final  String EDGES_PATH = "/Users/mihir/Documents/MEng folder new/LSI/Project2/input/edges.txt";
+	public static final String BLOCK_PATH = "/Users/mihir/Documents/MEng folder new/LSI/Project2/input/blocks.txt";
 
-	public static final  String EDGES_PATH = "/Users/Shiva/Documents/Masters/Assignements/LSI/proj2/input/edgestest.txt";
-	public static final String BLOCK_PATH = "/Users/Shiva/Documents/Masters/Assignements/LSI/proj2/input/blocks.txt";
+	public static final String FILTERED_EDGES = "/Users/mihir/Documents/MEng folder new/LSI/Project2/input/filteredEdges.txt";
+	public static final String FILTERED_EDGES_DEGREE = "/Users/mihir/Documents/MEng folder new/LSI/Project2/input/filteredEdgesAdjList.txt";
+	
+	
+	//public static final  String EDGES_PATH = "/Users/Shiva/Documents/Masters/Assignements/LSI/proj2/input/edgestest.txt";
+	//public static final String BLOCK_PATH = "/Users/Shiva/Documents/Masters/Assignements/LSI/proj2/input/blocks.txt";
 
 
 }
